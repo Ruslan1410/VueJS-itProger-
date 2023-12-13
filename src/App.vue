6205f71cff9152793ab02c04fbbda7b7d360cb7a
Ruslan <ruslan.leonoff2014@yandex.ru>
2023-12-13 11:20:30 +0300

    my first project

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  data(){
+    return {
+      info: 'Title',
+      someInfo: 'Anons of message'
+    }
+  },
+  methods: {
+    userData(word= 'New text') {
+     
+      // this.info = 'Some new'
+      this.someInfo = word
+    }
+  },
+}
+</script>
+
+<template>
+  <h3>{{ info }}</h3>
+  <p>{{ someInfo }}</p>
+
+  <button type ="button" @click="userData()">Отправить</button>
+</template>
+
+<style scoped>
+h3{
+  font-weight: lighter;
+}
+
+p {
+    color: rgb(222, 66, 66);
+}
+</style>
