02b2482503f3f03d8fc2d5f0d604ffe5269bbbfd
Ruslan <ruslan.leonoff2014@yandex.ru>
2023-12-16 12:46:52 +0300

    приложение погоды

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,75 @@
 <template>
-    <input type ="text" v-model="userName" placeholder="Имя">
-    <input type ="password" v-model="userPass" placeholder = "Пароль">
-    <input type ="email" v-model="userEmail" placeholder = "Email">
-    <p className="error">{{ error }}</p>
-    <button @click ="sendData()">Отправить</button>
-    
-    <div v-if="users.length == 0" className="user">
-      у нас нет пользователей
-    </div>
-    <div v-else-if="users.length == 1" className="user">
-     users has 1 element
-    </div>
-    <div v-else className="user">
-      users has more than 1 element
-    </div>
-    <User v-for="(el, index) in users" :key="index" :user="el" :index="index" :deleteUser="deleteUser"/>
-   
+<div class="wrapper">
+<h1>Погодное приложение</h1>
+<p>Узнать погоду в {{ city }}</p>
+<input type="text" v-model="city" placeholder="Введите город">
+<button>Получить погоду</button>
+</div>
+
 </template>
 
 
 <script>
-import User from './components/User.vue'
-
 export default {
-  components: { User },
-  data(){
+  data() {
     return {
-      users: [],
-      error: '',
-      userName:'',
-      userName:'',
-      userName:''
+      city: ""
     }
   },
-  methods: {
-      sendData() {
-        if(this.userName == ' ') {
-          this.error = 'ne verno';
-          return;
-        } else if(this.userEmail == ' '){
-          this.error = 'Email ne verno';
-          return;
-        } else if(this.userPass == ' '){
-          this.error = 'Pasee ne verno';
-          return;
-        }
-
-          this.error = '';
-
-        this.users.push({
-          name: this.userName,
-          pass: this.userPass,
-          email: this.userPass
-        })
-      },
-      deleteUser(index){
-        this.users.splice(index, 1);
-      }
-  }
 }
 </script>
 
 
 
 <style scoped>
+.wrapper {
+  width: 900px;
+  height: 500px;
+  border-radius: 50px;
+  padding: 20px;
+  background: #010a0a;
+  text-align: center;
+  color: #fff;
+}
 
+.wrapper h1 {
+  margin-top: 50px;
+
+}
+
+.wrapper p {
+  margin-top: 20px;
+}
+
+.wrapper input {
+  margin-top: 30px;
+  background: transparent;
+  border: 0;
+  border-bottom: 2px solid #110813;
+  color: #fcfcfc;
+  font-size: 14px;
+  padding: 5px 8px;
+  outline: none;
+
+}
+
+.wrapper input:focus {
+  border-bottom-color: #6e2d7d;
+}
+
+.wrapper button {
+  background: #e3bc4b;
+  color: #fff;
+  border-radius: 10px;
+  border: 2px solid #b99935;
+  padding: 10px 15px;
+  margin-left: 20px;
+  cursor: pointer;
+  transition: transform 50ms ease;
+
+}
+
+.wrapper button:hover {
+  transform: scale(1.1) translateY(-5px);
+}
 </style>
